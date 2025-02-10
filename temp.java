28bed5a517a880f46147de0e3e1717eaee0deae8
chetan <vaishnavchetan701@gmail.com>
2025-02-10 10:04:00 +0530

    first code of java

--- temp.java
+++ temp.java
@@ -1,5 +1,10 @@
-public class HelloWorld {
+class temp {  // Class name changed to 'temp' to match file name
     public static void main(String[] args) {
+        temp obj = new temp();
+        obj.sayHello();
+    }
+
+    void sayHello() {
         System.out.println("Hello, World!");
     }
 }
